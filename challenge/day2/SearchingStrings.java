1b9cf3a654e5d35576a98af5b0928de7341b3d4a
Christian Kyle <cautor3@gmail.com>
2025-09-04 22:35:20 +0800

    abi kog di nako mahuman ron - finally done the logic of Search String

--- challenge/day2/SearchingStrings.java
+++ challenge/day2/SearchingStrings.java
@@ -1,6 +1,6 @@
 package challenge.day2;
 
-import java.util.Scanner;
+import java.util.HashMap;
 
 public class SearchingStrings {
 	
@@ -11,25 +11,56 @@ public class SearchingStrings {
 	 * Success Criteria: Program finds all occurrences correctly.
 	 */
 	
-//	public static int indexOf(String search) {
-//		
-//		return 0;
-//	}
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void wordFrequencyCounter(String val) {
+		String[] words = val.split("[^a-zA-Z0-9]+");
 		
-		String myStr = "Hello planet earth, you are a great planet.";
+		HashMap<String, Integer> wordCounter = new HashMap<String, Integer>(); 
 		
-		String toFind = "";
-		
-		for(int i = 0; i < myStr.length(); i++) {
-			System.out.println(i +" "+ myStr.charAt(i));
-			toFind += myStr.charAt(i);
-			if("planet".equals(toFind)) {
-				System.out.println("FOUND!");
-				break;
+		for(int i = 0; i < words.length; i++) {			
+			if(!wordCounter.containsKey(words[i])) {
+				wordCounter.put(words[i], 1);				
+			}else {
+				int countRepeatedWords = wordCounter.get(words[i]);
+				wordCounter.replace(words[i], countRepeatedWords+=1);
 			}
 		}
+		
+		wordCounter.forEach((word, count) -> {
+			System.out.println(word +" -> "+ count);
+		});
+	}
+	
+	public static int indexOf(String str, String search, int fromIndex) {
+		for(int i = fromIndex; i < str.length(); i++) {
+			
+			String foundSearch = "";
+			
+			for(int j = 0; j < search.length(); j++) {
+				try {
+					if(search.charAt(j) == str.charAt(j + i)) {
+						foundSearch += str.charAt(j + i);
+					}else {
+						foundSearch = "";
+					}					
+				}catch(StringIndexOutOfBoundsException e) {
+					return -1;
+				}
+			}
+			
+			if(foundSearch.equals(search)) {
+				return i;
+			}
+		
+		}
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		String myStr = "Hello planet earth, you are a great planet.";
+		
+		System.out.println(indexOf(myStr, "planet", 1));
+		
+		wordFrequencyCounter(myStr);
 	}
 }
